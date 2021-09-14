217323ac69ace1f3c2d64802fb6c4348b32d272b
shoaibali7690 <shoaibali7690@gmail.com>
2021-09-13 23:09:48 -0400

    RemoveEquipmentOnCarton

---
+++ CandidateProject/Controllers/CartonController.cs
@@ -0,0 +1,325 @@
+﻿using CandidateProject.EntityModels;
+using CandidateProject.ViewModels;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Net;
+using System.Web.Mvc;
+
+namespace CandidateProject.Controllers
+{
+    public class CartonController : Controller
+    {
+        private CartonContext db = new CartonContext();
+
+        // GET: Carton
+        public ActionResult Index()
+        {
+         var cartons = db.Cartons
+        .Select(c => new CartonViewModel
+        {
+            Id = c.Id,
+            CartonNumber = c.CartonNumber,
+            EquipmentCount = c.CartonDetails.Count()
+        }).ToList();
+
+            return View(cartons);
+        }
+
+        // GET: Carton/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            var carton = db.Cartons
+                .Where(c => c.Id == id)
+                .Select(c => new CartonViewModel()
+                {
+                    Id = c.Id,
+                    CartonNumber = c.CartonNumber
+                })
+                .SingleOrDefault();
+            if (carton == null)
+            {
+                return HttpNotFound();
+            }
+            return View(carton);
+        }
+
+        // GET: Carton/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Carton/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,CartonNumber")] Carton carton)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Cartons.Add(carton);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(carton);
+        }
+
+        // GET: Carton/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            var carton = db.Cartons
+                .Where(c => c.Id == id)
+                .Select(c => new CartonViewModel()
+                {
+                    Id = c.Id,
+                    CartonNumber = c.CartonNumber
+                })
+                .SingleOrDefault();
+            if (carton == null)
+            {
+                return HttpNotFound();
+            }
+            return View(carton);
+        }
+
+        // POST: Carton/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,CartonNumber")] CartonViewModel cartonViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                var carton = db.Cartons.Find(cartonViewModel.Id);
+                carton.CartonNumber = cartonViewModel.CartonNumber;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(cartonViewModel);
+        }
+
+        // GET: Carton/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Carton carton = db.Cartons.Find(id);
+            if (carton == null)
+            {
+                return HttpNotFound();
+            }
+            
+            return View(carton);
+        }
+
+        public ActionResult DeleteAll(int cartonId)
+        {
+            int changesCount;
+
+            try
+            {
+                db.CartonDetails.RemoveRange(db.CartonDetails.Where(temp => temp.CartonId == cartonId));
+                changesCount = db.SaveChanges();
+
+                if (changesCount >= 0)
+                    return Content(string.Format("<script language='javascript'>window.alert('{0} Equiments has been deleted. Now you will be redirect to home page.')" +
+                        ";window.location='Index';</script>", changesCount));
+            }
+            catch (System.Exception ex)
+            {
+                string errorMessages = ex.InnerException.InnerException.ToString();
+                throw ex.InnerException.InnerException;
+            }
+            
+            return Content("<script>alert('Unknown error occurred. please try again later.');</script>");
+        }
+
+        // POST: Carton/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Carton carton = db.Cartons.Find(id);
+
+            if (carton == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                var cartonDetail = db.CartonDetails.Where(temp => temp.CartonId == id).ToList();
+
+                if (cartonDetail.Count > 0)
+                {
+                    string errorMessage = string.Format("You cannot delete this Catron because there are  {0} equipment in it.", cartonDetail.Count);
+
+                    ViewBag.Error = errorMessage;
+                    return View(carton);
+                }
+            }
+            db.Cartons.Remove(carton);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        public ActionResult AddEquipment(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            var carton = db.Cartons
+                .Where(c => c.Id == id)
+                .Select(c => new CartonDetailsViewModel()
+                {
+                    CartonNumber = c.CartonNumber,
+                    CartonId = c.Id
+                })
+                .SingleOrDefault();
+
+            if (carton == null)
+            {
+                return HttpNotFound();
+            }
+
+            if (id != null)
+            {
+                var equipmentCount = db.Equipments
+              .Where(e => db.CartonDetails.Where(cd => cd.CartonId == id).Select(cd => cd.EquipmentId).Contains(e.Id)).Count();
+
+
+                if (equipmentCount >= 10)
+                {
+                    string errorMessage = string.Format("Sorry, you cannot add more than 10 Equipment’s.");
+
+                    ViewBag.Error = errorMessage;
+                    return View(carton);
+                }
+            }
+
+            var equipment = db.Equipments
+                .Where(e => !db.CartonDetails.Select(cd => cd.EquipmentId).Contains(e.Id))
+                .Select(e => new EquipmentViewModel()
+                {
+                    Id = e.Id,
+                    ModelType = e.ModelType.TypeName,
+                    SerialNumber = e.SerialNumber
+                })
+                .ToList();
+
+            carton.Equipment = equipment;
+            return View(carton);
+        }
+
+        public ActionResult AddEquipmentToCarton([Bind(Include = "CartonId,EquipmentId")] AddEquipmentViewModel addEquipmentViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                var carton = db.Cartons
+                    .Include(c => c.CartonDetails)
+                    .Where(c => c.Id == addEquipmentViewModel.CartonId)
+                    .SingleOrDefault();
+                if (carton == null)
+                {
+                    return HttpNotFound();
+                }
+                var equipment = db.Equipments
+                    .Where(e => e.Id == addEquipmentViewModel.EquipmentId)
+                    .SingleOrDefault();
+                if (equipment == null)
+                {
+                    return HttpNotFound();
+                }
+                var detail = new CartonDetail()
+                {
+                    Carton = carton,
+                    Equipment = equipment
+                };
+
+                carton.CartonDetails.Add(detail);
+                db.SaveChanges();
+            }
+            return RedirectToAction("AddEquipment", new { id = addEquipmentViewModel.CartonId });
+        }
+
+        public ActionResult ViewCartonEquipment(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            var carton = db.Cartons
+                .Where(c => c.Id == id)
+                .Select(c => new CartonDetailsViewModel()
+                {
+                    CartonNumber = c.CartonNumber,
+                    CartonId = c.Id,
+                    Equipment = c.CartonDetails
+                        .Select(cd => new EquipmentViewModel()
+                        {
+                            Id = cd.EquipmentId,
+                            ModelType = cd.Equipment.ModelType.TypeName,
+                            SerialNumber = cd.Equipment.SerialNumber
+                        })
+                })
+                .SingleOrDefault();
+            if (carton == null)
+            {
+                return HttpNotFound();
+            }
+            return View(carton);
+        }
+        //Last remaining development item for this iteration:
+        //1. Implement the RemoveEquipmentOnCarton action on the CartonController, right now it is just throwing a Bad Request.
+        public ActionResult RemoveEquipmentOnCarton([Bind(Include = "CartonId,EquipmentId")] RemoveEquipmentViewModel removeEquipmentViewModel)
+        {
+            //return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            if (ModelState.IsValid
+                && removeEquipmentViewModel.CartonId != null
+                && removeEquipmentViewModel.EquipmentId !=null
+            )
+            {
+                CartonDetail cartonDetail = db.CartonDetails.Where(
+                    temp => temp.CartonId == removeEquipmentViewModel.CartonId
+                    && temp.EquipmentId == removeEquipmentViewModel.EquipmentId
+                ).FirstOrDefault();
+
+                if (cartonDetail != null)
+                {
+                    db.CartonDetails.Remove(cartonDetail);
+                    db.SaveChanges();
+                }
+                else
+                {
+                    return HttpNotFound();
+                }
+            }
+            return RedirectToAction("ViewCartonEquipment", new { id = removeEquipmentViewModel.CartonId });
+        }
+    }
+}
---
+++ CandidateProject/ViewModels/CartonViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace CandidateProject.ViewModels
+{
+    public class CartonViewModel
+    {
+        public int Id { get; set; }
+        public string CartonNumber { get; set; }
+
+        public int EquipmentCount { get; set; }
+
+    }
+}
